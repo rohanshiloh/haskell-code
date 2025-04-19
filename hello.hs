104ca2246bd2b932e3c00d2db2d7df56923e7dd7
Ro <rohanshilohshah@outlook.com>
2025-04-19 17:50:58 +0530

    haskell vectors

--- hello.hs
+++ hello.hs
@@ -1,4 +1,3 @@
-
 -- Enter distraction-free Zen mode: Command+K Z
 -- Open Keyboard Shortcuts: Command+K Command+S
 
@@ -19,6 +18,8 @@ main = do
     putStrLn "The first 10 Fibonacci numbers are:"
     print (fibonacci 10)
     rohanFunction "Hello, Rohan!"
+
+    exampleQ
 
 rohanFunction :: String -> IO ()
 rohanFunction str = do
@@ -55,4 +56,57 @@ instance Traversable Q where
         where
             concatL (QAtom a) (QList qas) = QList ((QAtom a):qas)
 
+-- Example usage of Functor, Foldable, and Traversable with Q
+exampleQ :: IO ()
+exampleQ = do
+    let q = QList [QAtom 1, QList [QAtom 2, QAtom 3], QAtom 4] :: Q Int
+    -- Show the structure
+    print q -- Output: QList [QAtom 1,QList [QAtom 2,QAtom 3],QAtom 4]
+    -- Show the structure with a different type
+    let q' = QList [QAtom 1.0, QList [QAtom 2.0, QAtom 3.0], QAtom 4.0] :: Q Double
+    print q' -- Output: QList [QAtom 1.0,QList [QAtom 2.0,QAtom 3.0],QAtom 4.0]
+    -- Show the structure with a different type
+    let q'' = QList [QAtom 'a', QList [QAtom 'b', QAtom 'c'], QAtom 'd'] :: Q Char
+    print q'' -- Output: QList [QAtom 'a',QList [QAtom 'b',QAtom 'c'],QAtom 'd']
+    -- Show the structure with a different type
+    let q''' = QList [QAtom "hello", QList [QAtom "world", QAtom "!"], QAtom ""] :: Q String
+    print q''' -- Output: QList [QAtom "hello",QList [QAtom "world",QAtom "!"],QAtom ""]
+    -- Show the structure with a different type
+    let q'''' = QList [QAtom True, QList [QAtom False, QAtom True], QAtom False] :: Q Bool
+    print q'''' -- Output: QList [QAtom True,QList [QAtom False,QAtom True],QAtom False]
+    -- Show the structure with a different type
+    let q''''' = QList [QAtom (1,2), QList [QAtom (3,4), QAtom (5,6)], QAtom (7,8)] :: Q (Int, Int)
+    print q''''' -- Output: QList [QAtom (1,2),QList [QAtom (3,4),QAtom (5,6)],QAtom (7,8)]
+
+    -- Functor: Apply a function to each element
+    print $ fmap (*2) q -- Output: QList [QAtom 2,QList [QAtom 4,QAtom 6],QAtom 8]
+
+    -- Functor: Apply a function to convert integers to strings
+    print $ fmap show q -- Output: QList [QAtom "1",QList [QAtom "2",QAtom "3"],QAtom "4"]
+
+    -- Foldable: Sum all elements
+    print $ foldr (+) 0 q -- Output: 10
+
+    -- Foldable: Count the number of elements
+    print $ foldr (\_ acc -> acc + 1) 0 q -- Output: 4
+    -- Foldable: Check if any element is greater than 2
+    print $ foldr (\x acc -> if x > 2 then True else acc) False q -- Output: True
+    -- Foldable: Check if all elements are greater than 0
+    print $ foldr (\x acc -> if x > 0 then True else acc) False q -- Output: True
+    -- Foldable: Find the maximum element
+    print $ foldr1 max q -- Output: 4
+    -- Foldable: Find the minimum element
+    print $ foldr1 min q -- Output: 1
+    -- Foldable: Check if the structure is empty
+    print $ null q -- Output: False
+
+    -- Traversable: Traverse the structure with a function that wraps elements in Maybe
+    print $ traverse (\x -> if x > 2 then Just x else Nothing) q 
+    -- Output: Nothing (because 1 and 2 are less than or equal to 2)
     
+    -- Traversable: Traverse the structure with a function that filters out odd numbers
+    print $ traverse (\x -> if even x then Just x else Nothing) q
+    -- Output: Nothing (because all elements are odd)
+
+
+
--- vector.hs
+++ vector.hs
@@ -1,25 +1,140 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-12.21 script
+-- https://academy.fpblock.com/haskell/library/vector/
+
 {-# LANGUAGE FlexibleContexts #-}
--- import           Control.Monad.Primitive     (PrimMonad, PrimState)
--- import qualified Data.ByteString.Lazy        as L
-import qualified Data.Vector                 as V
+import           Control.Monad.Primitive     (PrimMonad, PrimState)
+import qualified Data.ByteString.Lazy        as L
 import qualified Data.Vector.Generic.Mutable as M
 import qualified Data.Vector.Unboxed         as U
 import           Data.Word                   (Word8)
+import qualified Data.Vector as V
+import qualified GHC.TypeNats as U
 
--- https://wiki.haskell.org/Numeric_Haskell:_A_Vector_Tutorial
--- https://www.schoolofhaskell.com/user/commercial/content/vector
-
-
+-- | This program reads bytes from stdin and counts the frequency of each byte.
+-- | It uses a mutable vector to store the counts, and then prints the results.
+-- | The program is designed to be efficient and works with large amounts of data.
+-- | It uses the `Data.Vector` library for efficient vector operations.
+-- | The program demonstrates the use of mutable vectors, lazy byte strings,
+-- | and the `PrimMonad` type class for working with mutable state in Haskell.
+-- | The program also includes examples of various vector operations such as
+-- | filtering, mapping, zipping, and taking elements from the vector.
+-- | The program is a good example of how to use the `vector` library in Haskell
+-- | to perform efficient operations on large collections of data.
 main :: IO ()
 main = do
-    putStrLn "Printing numbers from 1 to 10:"
 
-    -- let a = fromList [10, 20, 30, 40]
-    -- print a
+    let list = [1..10] :: [Int]
+        vector = V.fromList list :: V.Vector Int
+        vector2 = V.enumFromTo 1 10 :: V.Vector Int
+    print $ vector == vector2 -- True
+    print $ list == V.toList vector -- also True
+    print $ V.filter odd vector -- 1,3,5,7,9
+    print $ V.map (* 2) vector -- 2,4,6,...,20
+    print $ V.zip vector vector -- (1,1),(2,2),...(10,10)
+    print $ V.zipWith (*) vector vector -- (1,4,9,16,...,100)
+    print $ V.reverse vector -- 10,9,...,1
+    print $ V.takeWhile (< 6) vector -- 1,2,3,4,5
+    print $ V.takeWhile odd vector -- 1
+    print $ V.takeWhile even vector -- []
+    print $ V.dropWhile (< 6) vector -- 6,7,8,9,10
+    print $ V.head vector -- 1
+    print $ V.tail vector -- 2,3,4,...,10
+    -- print $ V.head $ V.takeWhile even vector -- exception! vector: index out of bounds (0,0)
 
-    -- Create a new 256-size mutable vector
+
+    -- Get all of the contents from stdin
+    lbs <- L.getContents
+    -- Print the length of the input
+    putStrLn $ "Length of input: " ++ show (L.length lbs)
+    -- Print the first 10 bytes of the input
+    putStrLn $ "First 10 bytes: " ++ show (L.take 10 lbs)
+    -- Print the last 10 bytes of the input
+    putStrLn $ "Last 10 bytes: " ++ show (L.take 10 (L.reverse lbs))
+
     -- Fill the vector with zeros
-    -- mutable <- M.replicate 256 0
+    mutable <- M.replicate 256 0
+
+    -- Add all of the bytes from stdin
+    addBytes mutable lbs
+
+    -- Freeze to get an immutable version
+    vector <- U.unsafeFreeze mutable
+
+    -- Print the frequency of each byte
+    -- In newer vectors: we can use imapM_
+    -- In older vectors: we can use zipWithM_
+    U.imapM_ printFreq vector
+    -- U.zipWithM_ is a bit more efficient than U.imapM_
+    U.zipWithM_ printFreq (U.enumFromTo 0 255) vector
+
+-- Exercise 1: Try out some other functions available in the Data.Vector module. 
+-- In particular, try some of the fold functions, which we haven't covered here.
+
+    -- Sum of all elements using foldl
+    print $ U.foldl (+) 0 vector -- Output: 55
+    -- Product of all elements using foldl
+    print $ U.foldl (*) 1 vector -- Output: 3628800
+    -- Maximum element using foldl1
+    print $ U.foldl1 max vector -- Output: 10
+    -- Minimum element using foldl1
+    print $ U.foldl1 min vector -- Output: 1
+    -- Sum of all elements using foldr
+    print $ U.foldr (+) 0 vector -- Output: 55
+    -- Product of all elements using foldr
+    print $ U.foldr (*) 1 vector -- Output: 3628800
+    -- Check if all elements are greater than 0 using all
+    print $ U.all (> 0) vector -- Output: True
+    -- Check if any element is greater than 5 using any
+    print $ U.any (> 5) vector -- Output: True
+    -- Count the number of even elements using foldl'
+    print $ U.foldl' (\acc x -> if even x then acc + 1 else acc) 0 vector -- Output: 5
+    -- Concatenate all elements into a string using foldr
+    print $ U.foldr (\x acc -> show x ++ acc) "" vector -- Output: "12345678910"
+
+-- Exercise 2: Try using the Functor, Foldable, and Traversable versions of functions with a vector
 
 
-    putStrLn "This is a test function"
+
+
+
+
+
+-- Exercise 3: Use an unboxed (or storable) vector instead of the boxed vectors we were using above. 
+-- What code did you have to change from the original example? Do all of your examples from exercise 2 still work?
+
+
+-- There are also a number of functions in the Data.Vector module with no corresponding function in Prelude. 
+-- Many of these are related to mutable vectors (which we'll cover shortly). Others are present to provide 
+-- more efficient means of manipulating a vector, based on their special in-memory representation.
+
+
+
+addBytes :: (PrimMonad m, M.MVector v Int)
+         => v (PrimState m) Int
+         -> L.ByteString
+         -> m ()
+addBytes v lbs = mapM_ (addByte v) (L.unpack lbs)
+
+addByte :: (PrimMonad m, M.MVector v Int)
+        => v (PrimState m) Int
+        -> Word8
+        -> m ()
+addByte v w = do
+    -- Read out the old count value
+    oldCount <- M.read v index
+    -- Write back the updated count value
+    M.write v index (oldCount + 1)
+  where
+    -- Indices in vectors are always Ints. Our bytes come in as Word8, so we
+    -- need to convert them.
+    index :: Int
+    index = fromIntegral w
+
+printFreq :: Int -> Int -> IO ()
+printFreq index count = putStrLn $ concat
+    [ "Frequency of byte "
+    , show index
+    , ": "
+    , show count
+    ]
